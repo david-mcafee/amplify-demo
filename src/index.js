9fa0c515eb169485146c4922a428840ac2f09f48
mcafd@amazon.com <mcafd@amazon.com>
2021-07-27 16:06:30 -0700

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Amplify from "./aws-exports";
-import awsExports from "./aws-exports";
-
-Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,4 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log());
+reportWebVitals(console.log);
