82a9b1f92a01489019a49646a605c395f96a0975
mcafd@amazon.com <mcafd@amazon.com>
2021-07-29 14:57:41 -0700

    implement graphql subscriptions

--- src/Todo.js
+++ src/Todo.js
@@ -4,6 +4,8 @@ import { createTodo, deleteTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
 import {
   Button,
+  Header,
+  Icon,
   Input,
   List,
   ListItem,
@@ -12,8 +14,9 @@ import {
   ListDescription,
 } from "semantic-ui-react";
 import { v4 as uuidv4 } from "uuid";
-
 import awsExports from "./aws-exports";
+import { onCreateTodo, onDeleteTodo } from "./graphql/subscriptions";
+
 Amplify.configure(awsExports);
 
 const initialState = { name: "", description: "" };
@@ -25,6 +28,37 @@ const Todo = () => {
   useEffect(() => {
     fetchTodos();
   }, []);
+
+  // Subscribe to onCreate updates
+  useEffect(() => {
+    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+      next: (todoData) => {
+        console.log(todoData);
+        const todo = todoData?.value?.data?.onCreateTodo;
+
+        // Only add todo if it doesn't already exist in state (i.e. another user)
+        if (todos.filter((t) => t.id === todo.id).length === 0) {
+          setTodos([...todos, todo]);
+        }
+      },
+    });
+
+    return () => subscription.unsubscribe();
+  }, [todos]);
+
+  // Subscribe to onDelete updates
+  useEffect(() => {
+    const subscription = API.graphql(graphqlOperation(onDeleteTodo)).subscribe({
+      next: (todoData) => {
+        console.log(todoData);
+        const todo = todoData?.value?.data?.onDeleteTodo;
+        // TODO: don't always perform this operation (i.e. another user has removed todo)
+        setTodos(todos.filter((t) => t.id !== todo.id));
+      },
+    });
+
+    return () => subscription.unsubscribe();
+  }, [todos]);
 
   function setInput(key, value) {
     setFormState({ ...formState, [key]: value });
@@ -51,9 +85,10 @@ const Todo = () => {
 
       const todo = { id: todoId, ...formState };
 
+      setTodos([...todos, todo]);
+
       setFormState(initialState);
 
-      setTodos([...todos, todo]);
       await API.graphql(graphqlOperation(createTodo, { input: { ...todo } }));
     } catch (err) {
       // If there was an error, fetch todos because local state is not correct
@@ -72,7 +107,7 @@ const Todo = () => {
 
   async function removeTodo(todoId) {
     try {
-      /* delete a todo */
+      // TODO:
       setTodos(todos.filter((todo) => todo.id !== todoId));
       await API.graphql(
         graphqlOperation(deleteTodo, { input: { id: todoId } })
@@ -87,7 +122,11 @@ const Todo = () => {
   return (
     <div style={styles.parentContainer}>
       <div style={styles.container}>
-        <h2>Amplify Todos</h2>
+        <Header as="h1" icon textAlign>
+          <Icon name="users" circular />
+          <Header.Content>Shared Todos</Header.Content>
+          <Header sub>Open in 2 tabs to test concurrent updates</Header>
+        </Header>
         <Input
           onChange={(event) => setInput("name", event.target.value)}
           value={formState.name}
@@ -99,9 +138,14 @@ const Todo = () => {
           placeholder="Description"
         />
         <Button onClick={addTodo}>Create Todo</Button>
-        <List as="ul">
+        <List>
           {todos.map((todo, index) => (
             <ListItem key={todo.id ? todo.id : index}>
+              <ListContent floated="right">
+                <Button onClick={() => removeTodo(todo.id)} icon circular>
+                  <Icon name="delete" color="red" />
+                </Button>
+              </ListContent>
               <ListContent>
                 <ListHeader>
                   <p>{todo.name}</p>
@@ -109,9 +153,6 @@ const Todo = () => {
                 <ListDescription>
                   <p>{todo.description}</p>
                 </ListDescription>
-                <Button color={"red"} onClick={() => removeTodo(todo.id)}>
-                  Delete
-                </Button>
               </ListContent>
             </ListItem>
           ))}
