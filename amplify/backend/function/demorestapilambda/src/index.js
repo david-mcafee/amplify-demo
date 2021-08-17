e2805d5bb55d8c63c503c1a09c3ef8bd3d6395da
mcafd@amazon.com <mcafd@amazon.com>
2021-08-17 10:39:20 -0700

    enable CORS requests

--- amplify/backend/function/demorestapilambda/src/index.js
+++ amplify/backend/function/demorestapilambda/src/index.js
@@ -1,15 +1,13 @@
-
-
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
+  // TODO implement
+  const response = {
+    statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
 };
