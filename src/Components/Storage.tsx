5db8533e549df7a6a6b221dfdf71a432baa5af4f
mcafd@amazon.com <mcafd@amazon.com>
2021-08-18 13:39:35 -0700

    update

--- src/Components/Storage.tsx
+++ src/Components/Storage.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Storage } from "aws-amplify";
 import { Button, Divider, Container, Header } from "semantic-ui-react";
 import Progress from "./Progress";
+import { v4 as uuidv4 } from "uuid";
 
 const initialState: number = 0;
+
+// Track all events
+Storage.configure({ track: true });
 
 const StorageDemo = () => {
   const [uploadProgress, setUploadProgress] = useState(initialState);
@@ -12,10 +16,17 @@ const StorageDemo = () => {
     Math.floor((loaded / total) * 100);
 
   async function upload() {
+    const fileUUID = uuidv4();
+
     try {
-      const response = await Storage.put("test.txt", "File content", {
-        metadata: { key: "value" }, // (map<String>) A map of metadata to store with the object in S3.
-      });
+      const response = await Storage.put(
+        `test-${fileUUID}.txt`,
+        "File content",
+        {
+          // track: true, // Can track a specific event
+          metadata: { id: fileUUID }, // (map<String>) A map of metadata to store with the object in S3.
+        }
+      );
       console.log(response);
     } catch (error) {
       console.log("Error uploading text file: ", error);
@@ -23,10 +34,12 @@ const StorageDemo = () => {
   }
 
   async function uploadFile(e: any) {
+    const fileUUID = uuidv4();
     const file = e.target.files[0];
     try {
-      const response = await Storage.put(file.name, file, {
-        metadata: { key: "value" }, // (map<String>) A map of metadata to store with the object in S3.
+      const response = await Storage.put(`${file.name}-${fileUUID}`, file, {
+        // track: true, // Can track a specific event
+        metadata: { id: fileUUID }, // (map<String>) A map of metadata to store with the object in S3.
         progressCallback(progress: any) {
           console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
           setUploadProgress(calculatePercent(progress.loaded, progress.total));
@@ -39,12 +52,22 @@ const StorageDemo = () => {
     }
   }
 
+  const inputRef = useRef<HTMLDivElement>(
+    null
+  ) as React.MutableRefObject<HTMLInputElement>;
+
   return (
     <Container style={styles.container}>
       <Header as="h1">Lambda test</Header>
       <Button onClick={upload}>Upload text file</Button>
       <Divider />
-      <input type="file" onChange={uploadFile} placeholder={"upload a file"} />
+      <Button
+        content="Choose File"
+        labelPosition="left"
+        icon="file"
+        onClick={() => inputRef.current.click()}
+      />
+      <input ref={inputRef} type="file" hidden onChange={uploadFile} />
       <Divider />
       <Progress uploadProgress={uploadProgress} />
     </Container>
