aee1c01c31f6b12c1b2614cc4290c6b9bc09d414
mcafd@amazon.com <mcafd@amazon.com>
2021-08-19 15:31:06 -0700

    finish DataStore demo

--- src/Components/Nav/index.tsx
+++ src/Components/Nav/index.tsx
@@ -23,7 +23,7 @@ const Nav = ({ username }: NavProps) => {
     <Menu className={navContainer}>
       <Menu.Item header>{`Welcome ${username}!`}</Menu.Item>
       <Menu.Item>
-        <Link to="/">Todo</Link>
+        <Link to="/">DataStore</Link>
       </Menu.Item>
       <Menu.Item>
         <Link to="/chatbot">Chatbot</Link>
--- src/Components/Todo/index.tsx
+++ src/Components/Todo/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { API, graphqlOperation } from "aws-amplify";
-import { createTodo, deleteTodo } from "../../graphql/mutations";
-import { listTodos } from "../../graphql/queries";
+import { API, DataStore, graphqlOperation } from "aws-amplify";
+// import { createTodo, deleteTodo } from "../../graphql/mutations";
+// import { listTodos } from "../../graphql/queries";
 import {
   Button,
   Header,
@@ -14,8 +14,9 @@ import {
   ListDescription,
 } from "semantic-ui-react";
 import { v4 as uuidv4 } from "uuid";
-import { onCreateTodo, onDeleteTodo } from "../../graphql/subscriptions";
+// import { onCreateTodo, onDeleteTodo } from "../../graphql/subscriptions";
 import { useStyles } from "./styles";
+import { Todo as TodoModel } from "../../models";
 
 type Todo = {
   readonly id: string;
@@ -24,7 +25,12 @@ type Todo = {
   readonly priority: string;
 };
 
-const initialState = { name: "", description: "", priority: "" };
+type InitialState = {
+  readonly name: string;
+  readonly description: string;
+  readonly priority: string;
+};
+const initialState: InitialState = { name: "", description: "", priority: "" };
 const initialTodoState: Array<Todo> = [];
 
 const TodoHome = () => {
@@ -38,47 +44,47 @@ const TodoHome = () => {
   }, []);
 
   // Subscribe to onCreate updates
-  useEffect(() => {
-    // https://github.com/aws-amplify/amplify-js/issues/7589
-    // @ts-ignore
-    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
-      // TODO: add type
-      next: (todoData: any) => {
-        console.log(todoData);
-        const todo = todoData?.value?.data?.onCreateTodo;
+  // useEffect(() => {
+  //   // https://github.com/aws-amplify/amplify-js/issues/7589
+  //   // @ts-ignore
+  //   const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+  //     // TODO: add type
+  //     next: (todoData: any) => {
+  //       console.log(todoData);
+  //       const todo = todoData?.value?.data?.onCreateTodo;
 
-        // Only add todo if it doesn't already exist in state (i.e. another user)
-        if (todos.filter((t) => t.id === todo.id).length === 0) {
-          setTodos([...todos, todo]);
-        }
-      },
-      error: (error: any) => {
-        console.log(error);
-      },
-    });
+  //       // Only add todo if it doesn't already exist in state (i.e. another user)
+  //       if (todos.filter((t) => t.id === todo.id).length === 0) {
+  //         setTodos([...todos, todo]);
+  //       }
+  //     },
+  //     error: (error: any) => {
+  //       console.log(error);
+  //     },
+  //   });
 
-    return () => subscription.unsubscribe();
-  }, []);
+  //   return () => subscription.unsubscribe();
+  // }, []);
 
   // Subscribe to onDelete updates
-  useEffect(() => {
-    // https://github.com/aws-amplify/amplify-js/issues/7589
-    // @ts-ignore
-    const subscription = API.graphql(graphqlOperation(onDeleteTodo)).subscribe({
-      // TODO: add type
-      next: (todoData: any) => {
-        console.log(todoData);
-        const todo = todoData?.value?.data?.onDeleteTodo;
-        // TODO: don't always perform this operation (i.e. another user has removed todo)
-        setTodos(todos.filter((t) => t.id !== todo.id));
-      },
-      error: (error: any) => {
-        console.log(error);
-      },
-    });
+  // useEffect(() => {
+  //   // https://github.com/aws-amplify/amplify-js/issues/7589
+  //   // @ts-ignore
+  //   const subscription = API.graphql(graphqlOperation(onDeleteTodo)).subscribe({
+  //     // TODO: add type
+  //     next: (todoData: any) => {
+  //       console.log(todoData);
+  //       const todo = todoData?.value?.data?.onDeleteTodo;
+  //       // TODO: don't always perform this operation (i.e. another user has removed todo)
+  //       setTodos(todos.filter((t) => t.id !== todo.id));
+  //     },
+  //     error: (error: any) => {
+  //       console.log(error);
+  //     },
+  //   });
 
-    return () => subscription.unsubscribe();
-  }, []);
+  //   return () => subscription.unsubscribe();
+  // }, []);
 
   function setInput(key: string, value: string) {
     setFormState({ ...formState, [key]: value });
@@ -86,9 +92,9 @@ const TodoHome = () => {
 
   async function fetchTodos() {
     try {
-      // TODO: add type
-      const todoData: any = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.listTodos.items;
+      // const todoData: any = await API.graphql(graphqlOperation(listTodos));
+      // const todos = todoData.data.listTodos.items;
+      const todos: any = await DataStore.query(TodoModel);
       setTodos(todos);
     } catch (err) {
       console.log("error fetching todos");
@@ -102,15 +108,20 @@ const TodoHome = () => {
       // Generate id so that you can optimistically update state, while still allowing
       // for update and delete, since those require ids. Alternative is to fetch
       // on each operation, but that's slow.
-      const todoId = uuidv4();
+      // const todoId = uuidv4();
 
-      const todo = { id: todoId, ...formState };
+      const todo = { id: uuidv4(), ...formState };
 
       setTodos([...todos, todo]);
 
       setFormState(initialState);
 
-      await API.graphql(graphqlOperation(createTodo, { input: { ...todo } }));
+      // await API.graphql(graphqlOperation(createTodo, { input: { ...todo } }));
+      await DataStore.save(
+        new TodoModel({
+          ...todo,
+        })
+      );
     } catch (err) {
       // If there was an error, fetch todos because local state is not correct
       fetchTodos();
@@ -126,13 +137,13 @@ const TodoHome = () => {
   //   })
   // );
 
-  async function removeTodo(todoId: string) {
+  async function removeTodo(todo: any) {
     try {
-      // TODO:
-      setTodos(todos.filter((todo) => todo.id !== todoId));
-      await API.graphql(
-        graphqlOperation(deleteTodo, { input: { id: todoId } })
-      );
+      setTodos(todos.filter((t) => t.id !== todo.id));
+      // await API.graphql(
+      //   graphqlOperation(deleteTodo, { input: { id: todoId } })
+      // );
+      await DataStore.delete(todo);
     } catch (err) {
       // If there was an error, fetch todos because local state is not correct
       fetchTodos();
@@ -146,7 +157,7 @@ const TodoHome = () => {
         <Header as="h1" icon textAlign="center">
           <Icon name="users" circular />
           <Header.Content>My Todos</Header.Content>
-          <Header sub>Open in 2 tabs to test concurrent updates</Header>
+          <Header sub>Simple Todo app using Amplify DataStore</Header>
         </Header>
         <Input
           onChange={(event) => setInput("name", event.target.value)}
@@ -168,7 +179,7 @@ const TodoHome = () => {
           {todos.map((todo, index) => (
             <ListItem key={todo.id ? todo.id : index}>
               <ListContent floated="right">
-                <Button onClick={() => removeTodo(todo.id)} icon circular>
+                <Button onClick={() => removeTodo(todo)} icon circular>
                   <Icon name="delete" color="red" />
                 </Button>
               </ListContent>
