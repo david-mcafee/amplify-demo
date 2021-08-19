3ae2519e9f53d6604189064eff6758aeb6c5f0c4
mcafd@amazon.com <mcafd@amazon.com>
2021-08-19 12:29:30 -0700

    update S3 Lambda trigger

--- amplify/backend/function/S3Trigger1105e06e/src/index.js
+++ amplify/backend/function/S3Trigger1105e06e/src/index.js
@@ -1,11 +1,22 @@
 /* Amplify Params - DO NOT EDIT
 	ENV
 	REGION
-Amplify Params - DO NOT EDIT */// eslint-disable-next-line
+Amplify Params - DO NOT EDIT */ // eslint-disable-next-line
 exports.handler = async function (event) {
-  console.log('Received S3 event:', JSON.stringify(event, null, 2));
+  console.log("Received S3 event:", JSON.stringify(event, null, 2));
   // Get the object from the event and show its content type
   const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
   const key = event.Records[0].s3.object.key; //eslint-disable-line
   console.log(`Bucket: ${bucket}`, `Key: ${key}`);
+
+  const response = {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*", // TODO
+    },
+    body: JSON.stringify("Received S3 event:", JSON.stringify(event, null, 2)),
+  };
+  return response;
 };
