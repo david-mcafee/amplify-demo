62dcee516bd22ddc67213ce35fc4d9d0b9e8fc94
David McAfee <mcafd@amazon.com>
2021-10-19 18:00:21 -0700

    test Storage.copy() for customer issue; update Lambda function

--- amplify/backend/function/demorestapilambda/src/index.js
+++ amplify/backend/function/demorestapilambda/src/index.js
@@ -1,13 +1,68 @@
+// exports.handler = async (event) => {
+//   // TODO implement
+//   const response = {
+//     statusCode: 200,
+//     //  Uncomment below to enable CORS requests
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Headers": "*", // TODO
+//     },
+//     body: JSON.stringify("Hello from Lambda!"),
+//   };
+//   return response;
+// };
+
+// https://blog.focusotter.com/send-an-sms-to-customers-using-react-and-aws-amplify
+/* Amplify Params - DO NOT EDIT
+    API_DELIVERYAPI_APIID
+    API_DELIVERYAPI_APINAME
+    ENV
+    REGION
+Amplify Params - DO NOT EDIT */
+const AWS = require("aws-sdk");
+const SNSClient = new AWS.SNS();
+
 exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*", // TODO
-    },
-    body: JSON.stringify("Hello from Lambda!"),
+  // 1. get orderID from the event
+  // const orderID = JSON.parse(event.body).orderID;
+
+  // 🪄 used orderID to look up orderDetails in database 🪄
+
+  const order = {
+    orderID: "123-abc",
+    address: "987 fakeaddress ln",
+    phoneNumber: "+12813308004", //replace with your phone #
   };
+
+  let response;
+
+  try {
+    // 2.  🎉 send order update to customer
+    const resp = await SNSClient.publish({
+      Message: "Your order is out for delivery",
+      TargetArn: process.env.SNS_TOPIC_ARN,
+      MessageAttributes: {
+        sms: {
+          DataType: "String.Array",
+          StringValue: JSON.stringify([order.phoneNumber]),
+        },
+      },
+    }).promise();
+    console.log(resp);
+    // 3. send response back to the frontend
+    response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+      body: JSON.stringify({
+        address: "SNS PUBLISHED",
+      }),
+    };
+  } catch (e) {
+    console.log(e);
+    // todo: update response for when things go bad 😢
+  }
   return response;
 };
--- src/Components/Storage/index.tsx
+++ src/Components/Storage/index.tsx
@@ -1,17 +1,37 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Storage } from "aws-amplify";
-import { Button, Divider, Container, Header } from "semantic-ui-react";
+import {
+  Button,
+  Divider,
+  Container,
+  Header,
+  Icon,
+  List,
+  ListContent,
+  ListHeader,
+  ListItem,
+} from "semantic-ui-react";
 import Progress from "../Progress";
 import { v4 as uuidv4 } from "uuid";
 import { useStyles } from "./styles";
 
+type File = {
+  key?: string;
+};
+
 const initialState: number = 0;
+const initialFileState: Array<File> = [];
 
 // Track all events
 Storage.configure({ track: true });
 
 const StorageDemo = () => {
   const [uploadProgress, setUploadProgress] = useState(initialState);
+  const [files, setFiles] = useState(initialFileState);
+
+  useEffect(() => {
+    fetchFiles();
+  }, []);
 
   const { container } = useStyles();
 
@@ -55,6 +75,31 @@ const StorageDemo = () => {
     }
   }
 
+  async function fetchFiles() {
+    try {
+      const result = await Storage.list("");
+      console.log(result);
+      setFiles(result);
+    } catch (error) {
+      console.log("error fetching files");
+    }
+  }
+
+  async function copyFile(file: File) {
+    try {
+      // Copies 'existing/srcKey' to 'copied/destKey' within the 'public' access level
+      const copied = await Storage.copy(
+        { key: `${file.key}` },
+        { key: `copied/${file.key}` }
+      );
+      console.log(copied);
+    } catch (error) {
+      console.log("error copying file", error);
+    } finally {
+      fetchFiles();
+    }
+  }
+
   const inputRef = useRef<HTMLDivElement>(
     null
   ) as React.MutableRefObject<HTMLInputElement>;
@@ -73,6 +118,22 @@ const StorageDemo = () => {
       <input ref={inputRef} type="file" hidden onChange={uploadFile} />
       <Divider />
       <Progress uploadProgress={uploadProgress} />
+      <List>
+        {files.map((file, index) => (
+          <ListItem key={file.key ? file.key : index}>
+            <ListContent floated="right">
+              <Button onClick={() => copyFile(file)} icon circular>
+                <Icon name="caret right" color="red" />
+              </Button>
+            </ListContent>
+            <ListContent>
+              <ListHeader>
+                <p>{file.key}</p>
+              </ListHeader>
+            </ListContent>
+          </ListItem>
+        ))}
+      </List>
     </Container>
   );
 };
