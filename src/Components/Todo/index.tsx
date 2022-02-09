8e35e7f0210ef1632b6a794a271da44320a8fba6
David McAfee <mcafd@amazon.com>
2022-02-09 15:26:54 -0800

    update todos

--- src/Components/Todo/index.tsx
+++ src/Components/Todo/index.tsx
@@ -1,12 +1,23 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { API, graphqlOperation, Hub } from "aws-amplify";
-// import { createTodo, deleteTodo } from "../../graphql/mutations";
-import { createTodo } from "../../graphql/mutations";
+import { createTodo, deleteTodo } from "../../graphql/mutations";
 import { listTodos } from "../../graphql/queries";
 import { v4 as uuidv4 } from "uuid";
-import { onCreateTodo, onDeleteTodo } from "../../graphql/subscriptions";
-import { useStyles } from "./styles";
+// import { onCreateTodo, onDeleteTodo } from "../../graphql/subscriptions";
+// import { useStyles } from "./styles";
 // import { Todo as TodoModel } from "../../models";
+import {
+  // Badge,
+  Button,
+  Collection,
+  Card,
+  Heading,
+  // Divider,
+  Flex,
+  // View,
+  // IconBookmark,
+  Text,
+} from "@aws-amplify/ui-react";
 
 type Todo = {
   readonly id: string;
@@ -25,54 +36,54 @@ const TodoHome = () => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState(initialTodoState);
 
-  const { container, parentContainer } = useStyles();
+  // const { container, parentContainer } = useStyles();
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
   // Subscribe to onCreate updates
-  useEffect(() => {
-    // https://github.com/aws-amplify/amplify-js/issues/7589
-    // @ts-ignore
-    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
-      // TODO: add type
-      next: (todoData: any) => {
-        console.log(todoData);
-        const todo = todoData?.value?.data?.onCreateTodo;
+  // useEffect(() => {
+  //   // https://github.com/aws-amplify/amplify-js/issues/7589
+  //   // @ts-ignore
+  //   const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+  //     // TODO: add type
+  //     next: (todoData: any) => {
+  //       console.log(todoData);
+  //       const todo = todoData?.value?.data?.onCreateTodo;
 
-        // Only add todo if it doesn't already exist in state (i.e. another user)
-        if (todos.filter((t) => t.id === todo.id).length === 0) {
-          setTodos([...todos, todo]);
-        }
-      },
-      error: (error: any) => {
-        console.log(error);
-      },
-    });
+  //       // Only add todo if it doesn't already exist in state (i.e. another user)
+  //       if (todos.filter((t) => t.id === todo.id).length === 0) {
+  //         setTodos([...todos, todo]);
+  //       }
+  //     },
+  //     error: (error: any) => {
+  //       console.log(error);
+  //     },
+  //   });
 
-    return subscription.unsubscribe;
-  }, []);
+  //   return subscription.unsubscribe;
+  // }, []);
 
   // Subscribe to onDelete updates
-  useEffect(() => {
-    // https://github.com/aws-amplify/amplify-js/issues/7589
-    // @ts-ignore
-    const subscription = API.graphql(graphqlOperation(onDeleteTodo)).subscribe({
-      // TODO: add type
-      next: (todoData: any) => {
-        console.log(todoData);
-        const todo = todoData?.value?.data?.onDeleteTodo;
-        // TODO: don't always perform this operation (i.e. another user has removed todo)
-        setTodos(todos.filter((t) => t.id !== todo.id));
-      },
-      error: (error: any) => {
-        console.log(error);
-      },
-    });
+  // useEffect(() => {
+  //   // https://github.com/aws-amplify/amplify-js/issues/7589
+  //   // @ts-ignore
+  //   const subscription = API.graphql(graphqlOperation(onDeleteTodo)).subscribe({
+  //     // TODO: add type
+  //     next: (todoData: any) => {
+  //       console.log(todoData);
+  //       const todo = todoData?.value?.data?.onDeleteTodo;
+  //       // TODO: don't always perform this operation (i.e. another user has removed todo)
+  //       setTodos(todos.filter((t) => t.id !== todo.id));
+  //     },
+  //     error: (error: any) => {
+  //       console.log(error);
+  //     },
+  //   });
 
-    return () => subscription.unsubscribe();
-  }, []);
+  //   return () => subscription.unsubscribe();
+  // }, []);
 
   // DataStore subscription
   // useEffect(() => {
@@ -108,7 +119,8 @@ const TodoHome = () => {
   async function fetchTodos() {
     try {
       const todoData: any = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.ulTodos.items;
+      console.log(todoData);
+      const todos = todoData?.data?.listTodos?.items;
       // const todos: any = await DataStore.query(TodoModel);
       setTodos(todos);
     } catch (err) {
@@ -129,7 +141,11 @@ const TodoHome = () => {
 
       setFormState(initialState);
 
-      await API.graphql(graphqlOperation(createTodo, { input: { ...todo } }));
+      const result: any = await API.graphql(
+        graphqlOperation(createTodo, { input: { ...todo } })
+      );
+
+      setTodos((todos) => [...todos, result?.data?.createTodo]);
       // await DataStore.save(
       //   new TodoModel({
       //     ...todo,
@@ -150,60 +166,66 @@ const TodoHome = () => {
   //   })
   // );
 
-  // async function removeTodo(todo: any) {
-  //   try {
-  //     setTodos(todos.filter((t) => t.id !== todo.id));
-  //     await API.graphql(
-  //       graphqlOperation(deleteTodo, { input: { id: todo.id } })
-  //     );
-  //     // await DataStore.delete(todo);
-  //   } catch (err) {
-  //     // If there was an error, fetch todos because local state is not correct
-  //     fetchTodos();
-  //     console.log("error deleting todo:", err);
-  //   }
-  // }
+  async function removeTodo(todo: any) {
+    try {
+      setTodos(todos.filter((t) => t.id !== todo.id));
+      const result = await API.graphql(
+        graphqlOperation(deleteTodo, { input: { id: todo.id } })
+      );
+      console.log(result);
+      // await DataStore.delete(todo);
+    } catch (err) {
+      // If there was an error, fetch todos because local state is not correct
+      fetchTodos();
+      console.log("error deleting todo:", err);
+    }
+  }
 
   return (
-    <div className={parentContainer}>
-      <div className={container}>
-        {/* <div as="h1" icon textAlign="center">
-          <Icon name="users" circular />
-          <div.Content>My Todos</div.Content>
-          <div sub>Amplify GraphQL API Demo</div>
-        </div> */}
-        <input
-          onChange={(event) => setinput("name", event.target.value)}
-          value={formState.name}
-          placeholder="Name"
-        />
-        <input
-          onChange={(event) => setinput("description", event.target.value)}
-          value={formState.description}
-          placeholder="Description"
-        />
-        <button onClick={addTodo}>Create Todo</button>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={todo.id ? todo.id : index}>
-              {/* <div floated="right"> */}
-              {/* <button onClick={() => removeTodo(todo)} icon circular>
-                  <Icon name="delete" color="red" />
-                </button> */}
-              {/* </div> */}
-              <div>
-                <div>
-                  <p>{todo.name}</p>
-                </div>
-                <div>
-                  <p>{todo.description}</p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <Card width={"100%"} variation={"elevated"}>
+      <Flex direction="column" alignItems="center">
+        <Card width={"100%"} variation={"elevated"}>
+          <Flex direction={"column"} alignItems="center">
+            <Heading level={1}>My Todos</Heading>
+            <Heading level={2}>Amplify GraphQL API Demo</Heading>
+          </Flex>
+        </Card>
+        <Card variation={"elevated"}>
+          <Flex direction={"column"}>
+            <input
+              onChange={(event) => setinput("name", event.target.value)}
+              value={formState.name}
+              placeholder="Name"
+            />
+            <input
+              onChange={(event) => setinput("description", event.target.value)}
+              value={formState.description}
+              placeholder="Description"
+            />
+            <button onClick={addTodo}>Create Todo</button>
+          </Flex>
+        </Card>
+        <Collection type="list" items={todos} gap="1.5rem">
+          {(todo, index) => (
+            <Card
+              key={todo.id ? todo.id : index}
+              padding="1.5rem"
+              variation={"elevated"}
+            >
+              <Heading level={4}>{todo.name}</Heading>
+              <Text>{todo.description}</Text>
+              <Button
+                loadingText="loading"
+                ariaLabel="Delete"
+                onClick={() => removeTodo(todo)}
+              >
+                Delete
+              </Button>
+            </Card>
+          )}
+        </Collection>
+      </Flex>
+    </Card>
   );
 };
 
