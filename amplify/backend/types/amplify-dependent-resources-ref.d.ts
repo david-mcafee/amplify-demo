ff7d4ea6eb6a967dc8b773a0c3a999bc3093adc1
David McAfee <mcafd@amazon.com>
2023-07-13 15:19:34 -0700

    update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,46 +1,48 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "amplifydemo": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "amplifydemo18fe868c": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "analytics": {
-        "amplifydemo2": {
-            "Region": "string",
-            "Id": "string",
-            "appName": "string"
-        }
-    },
-    "function": {
-        "demorestapilambda": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "S3Trigger1105e06e": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "storage": {
-        "amplifydemostorage": {
-            "BucketName": "string",
-            "Region": "string"
-        }
+  "analytics": {
+    "amplifydemo2": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
     }
+  },
+  "api": {
+    "amplifydemo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifydemo18fe868c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger1105e06e": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "demorestapilambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "amplifydemostorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
